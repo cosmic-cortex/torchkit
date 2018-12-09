0347cf2ab3aa84c22b54df2a744ce331357d98c1
cosmic-cortex <theodore.danka@gmail.com>
2018-12-09 13:36:34 +0100

    device default changed from none to cpu

--- torchkit/utils/model.py
+++ torchkit/utils/model.py
@@ -3,17 +3,16 @@ import numpy as np
 import torch
 import torch.nn as nn
 
-from skimage import io
-
 from torch.autograd import Variable
 from torch.utils.data import DataLoader
 
 from torchkit.utils.misc import chk_mkdir
 
+
 class Model:
     def __init__(self, net: nn.Module, loss, optimizer, checkpoint_folder: str,
                  scheduler: torch.optim.lr_scheduler._LRScheduler = None,
-                 device: torch.device = None):
+                 device: torch.device = torch.device('cpu')):
         """
         Wrapper for PyTorch models.
 
@@ -24,7 +23,7 @@ class Model:
             checkpoint_folder: Folder for saving the results and predictions.
             scheduler: Learning rate scheduler for the optimizer. Optional.
             device: The device on which the model and tensor should be
-                located. Optional.
+                located. Optional. The default device is the cpu.
 
         Attributes:
             net: PyTorch model.
@@ -42,14 +41,13 @@ class Model:
         self.checkpoint_folder = checkpoint_folder
         chk_mkdir(self.checkpoint_folder)
 
+        # moving net and loss to the selected device
         self.device = device
-        if self.device:
-            self.net.to(device=self.device)
-            self.loss.to(device=self.device)
+        self.net.to(device=self.device)
+        self.loss.to(device=self.device)
 
     def train_model(self, dataset, n_epochs, n_batch=1, verbose=False,
-                    validation_dataset=None, prediction_dataset=None,
-                    save_freq=100):
+                    validation_dataset=None, save_freq=100):
         self.net.train(True)
 
         min_loss = np.inf
@@ -59,11 +57,8 @@ class Model:
             epoch_running_loss = 0
             for batch_idx, (X_batch, y_batch, name) in enumerate(DataLoader(dataset, batch_size=n_batch, shuffle=True)):
 
-                if self.device:
-                    X_batch = Variable(X_batch.to(device=self.device))
-                    y_batch = Variable(y_batch.to(device=self.device))
-                else:
-                    X_batch, y_batch = Variable(X_batch), Variable(y_batch)
+                X_batch = Variable(X_batch.to(device=self.device))
+                y_batch = Variable(y_batch.to(device=self.device))
 
                 # training
                 self.optimizer.zero_grad()
@@ -75,17 +70,19 @@ class Model:
                 epoch_running_loss += training_loss.item()
 
                 if verbose:
-                    print('(Epoch no. %d, batch no. %d) loss: %f' % (epoch_idx, batch_idx, training_loss.item()))
+                    print('(Epoch no. %d, batch no. %d) loss: %f' % (epoch_idx, batch_idx + 1, training_loss.item()))
 
             total_running_loss += epoch_running_loss/(batch_idx + 1)
-            print('(Epoch no. %d) loss: %f' % (epoch_idx, epoch_running_loss/(batch_idx + 1)))
+            if verbose:
+                print('(Epoch no. %d) loss: %f' % (epoch_idx +1, epoch_running_loss/(batch_idx + 1)))
 
             if validation_dataset is not None:
                 validation_error = self.validate(validation_dataset, n_batch=1)
                 if validation_error < min_loss:
                     torch.save(self.net.state_dict(), os.path.join(self.checkpoint_folder, 'model'))
-                    print('Validation loss improved from %f to %f, model saved to %s'
-                          % (min_loss, validation_error, self.checkpoint_folder))
+                    if verbose:
+                        print('Validation loss improved from %f to %f, model saved to %s'
+                              % (min_loss, validation_error, self.checkpoint_folder))
                     min_loss = validation_error
 
                 if self.scheduler is not None:
@@ -113,40 +110,35 @@ class Model:
 
         return total_running_loss/n_batch
 
-    def validate(self, dataset, n_batch=1):
+    def validate(self, dataset, n_batch=1, verbose=False):
         self.net.train(False)
 
         total_running_loss = 0
         for batch_idx, (X_batch, y_batch, name) in enumerate(DataLoader(dataset, batch_size=n_batch, shuffle=False)):
 
-            if self.device:
-                X_batch = Variable(X_batch.to(device=self.device))
-                y_batch = Variable(y_batch.to(device=self.device))
-            else:
-                X_batch, y_batch = Variable(X_batch), Variable(y_batch)
+            X_batch = Variable(X_batch.to(device=self.device))
+            y_batch = Variable(y_batch.to(device=self.device))
 
             y_out = self.net(X_batch)
             training_loss = self.loss(y_out, y_batch)
 
             total_running_loss += training_loss.item()
 
-        print('Validation loss: %f' % (total_running_loss / (batch_idx + 1)))
+        if verbose:
+            print('Validation loss: %f' % (total_running_loss / (batch_idx + 1)))
+
         self.net.train(True)
 
         del X_batch, y_batch
 
         return total_running_loss/(batch_idx + 1)
 
-    def predict(self, dataset, export_path, channel=None):
+    def predict(self, dataset, export_path):
         self.net.train(False)
         chk_mkdir(export_path)
 
         for batch_idx, (X_batch, image_filename) in enumerate(DataLoader(dataset, batch_size=1)):
-            if self.device:
-                X_batch = Variable(X_batch.to(device=self.device))
-                y_out = self.net(X_batch).cpu().data.numpy()
-            else:
-                X_batch = Variable(X_batch)
-                y_out = self.net(X_batch).data.numpy()
+            X_batch = Variable(X_batch.to(device=self.device))
+            y_out = self.net(X_batch).cpu().data.numpy()
 
-                io.imsave(os.path.join(export_path, image_filename[0]), y_out[0, :, :, :].transpose((1, 2, 0)))
+            io.imsave(os.path.join(export_path, image_filename[0]), y_out[0, :, :, :].transpose((1, 2, 0)))
